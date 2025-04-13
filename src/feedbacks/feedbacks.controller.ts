d9ebebe1bcfd5624640ab55bb1f2e7eb5c591f54
yurkenchik <yura.ilchshyn@gmail.com>
2025-04-13 14:43:30 +0300

    Created feedbacks feature

--- src/feedbacks/feedbacks.controller.ts
+++ src/feedbacks/feedbacks.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, Query, UseGuards } from '@nestjs/common';
+import { UserId } from '../decorators/user-id.decorator';
+import { CreateFeedbackDto } from './dto/create-feedback.dto';
+import { Feedback } from './feedbacks.entity';
+import { FeedbacksService } from './feedbacks.service';
+import { ApiBearerAuth, ApiBody, ApiParam, ApiQuery, ApiResponse } from '@nestjs/swagger';
+import { AuthGuard } from '../auth/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('feedbacks')
-export class FeedbacksController {}
+export class FeedbacksController {
+    constructor(private feedbackService: FeedbacksService) {}
+
+    @ApiResponse({ type: Feedback, status: HttpStatus.CREATED })
+    @ApiBody({ type: CreateFeedbackDto })
+    @ApiQuery({ name: 'locationId', type: String })
+    @ApiBearerAuth()
+    @Post()
+    async createFeedback(
+        @UserId() userId: string,
+        @Query() locationId: string,
+        @Body() createFeedbackDto: CreateFeedbackDto
+    ): Promise<Feedback> {
+        return this.feedbackService.createFeedback(userId, locationId, createFeedbackDto);
+    }
+
+    @ApiResponse({ type: [Feedback], status: HttpStatus.OK })
+    @ApiParam({ name: 'locationId', type: String })
+    @ApiBearerAuth()
+    @Get('/location/:locationId')
+    async getFeedbacksByLocation(@Param('locationId') locationId: string): Promise<Array<Feedback>> {
+        return this.feedbackService.getFeedbacksByLocation(locationId);
+    }
+}
--- src/feedbacks/feedbacks.service.ts
+++ src/feedbacks/feedbacks.service.ts
@@ -1,24 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Feedback } from './feedbacks.entity';
 import { UsersService } from '../users/users.service';
 import { LocationsService } from '../locations/locations.service';
+import { CreateFeedbackDto } from './dto/create-feedback.dto';
+import { ExceptionMessage } from '../utils/exception-message.enum';
 
 @Injectable()
 export class FeedbacksService {
     constructor(
         @InjectRepository(Feedback)
-        private readonly locationRepository: Repository<Feedback>,
+        private readonly feedbackRepository: Repository<Feedback>,
         private readonly usersService: UsersService,
         private readonly locationsService: LocationsService,
     ) {}
 
-    // async createFeedback(
-    //     userId: string,
-    //     locationId: string,
-    //     createFeedbackDto: CreateFeedbackDto,
-    // ): Promise<Feedback> {
-    //     const user = await this.usersService
-    // }
+    async createFeedback(
+        userId: string,
+        locationId: string,
+        createFeedbackDto: CreateFeedbackDto,
+    ): Promise<Feedback> {
+        const user = await this.usersService.getUser(userId);
+        if (user) {
+            throw new BadRequestException(ExceptionMessage.USER_ALREADY_LEFT_FEEDBACK);
+        }
+
+        const location = await this.locationsService.getLocationById(locationId);
+
+        const feedbackInsertResult = await this.feedbackRepository
+            .createQueryBuilder()
+            .insert()
+            .into(Feedback)
+            .values({
+                ...createFeedbackDto,
+                user,
+                location
+            })
+            .execute();
+
+        const feedbackFormatResponse = feedbackInsertResult.raw[0] as Feedback;
+        const feedbackResponse = await this.feedbackRepository.save(feedbackFormatResponse);
+
+        const locationFeedbacks = await this.getFeedbacksByLocation(locationId);
+
+        if (locationFeedbacks && locationFeedbacks.length) {
+            let totalRating = 0;
+            for (const feedback of locationFeedbacks) {
+                totalRating += feedback.rate;
+            }
+
+            const averageRating = totalRating / locationFeedbacks.length;
+            location.rating = averageRating;
+            await this.locationsService.save(location);
+        }
+
+        return feedbackResponse;
+    }
+
+    async getFeedbacksByLocation(locationId: string): Promise<Array<Feedback>> {
+        const location = await this.locationsService.getLocationById(locationId);
+
+        return this.feedbackRepository
+            .createQueryBuilder()
+            .where('locationId := location', { locationId: location.id })
+            .getMany();
+    }
 }
